a59fe86a20ac2fe6719f6157bd174954cf2d901f
webhacking <a@hax0r.info>
2019-04-12 16:00:26 +0900

    Solved

--- custom-operator.ts
+++ custom-operator.ts
@@ -1,9 +1,10 @@
 import {Observable, pipe} from 'rxjs';
 import {Point} from './lib/test-source';
 import {UnaryFunction} from 'rxjs/interfaces';
+import {distinctUntilChanged} from "rxjs/operators";
 
 export function filterClosePoints(distance: number): UnaryFunction<Observable<Point>, Observable<Point>> {
   return pipe(
-    // TODO
+    distinctUntilChanged((p1, p2) => Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2) < distance)
   );
 }
